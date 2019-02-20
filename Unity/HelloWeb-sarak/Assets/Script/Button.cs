125f7d704d46137ff5ae578c131f38e78a40000d
sara1204 <knezevicsara@gmx.at>
2019-02-20 16:00:11 +0100

    add Unity

---
+++ Unity/HelloWeb-sarak/Assets/Script/Button.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+string button ="HelloWeb";
+    
+    public void ClickMe() 
+    {
+        Debug.Log(button);
+    }
+
+	void Start () {
+		
+    
+        
+	}
+	
+	
+	void Update () {
+		
+	}
+}
